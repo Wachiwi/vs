3e9bd216c5112db3157c56d9d3f57ddd0171d9f2
David Schwarzmann <dev@dschwarz.codes>
2017-06-20 16:36:19 +0200

    Minor Changes

--- blatt04/a4/src/rmilernen/client/Client.java
+++ blatt04/a4/src/rmilernen/client/Client.java
@@ -32,6 +32,46 @@ public class Client {
         return String.copyValueOf(s);
     }
 
+    private static void call_basic(String namespace, int port, int c) {
+        Registry registry = null;
+        BasicIf basic = null;
+
+        try {
+            registry = LocateRegistry.getRegistry(port);
+            basic = (BasicIf) registry.lookup(  "a4-" + namespace  );
+        } catch (RemoteException | NotBoundException e) {
+            System.err.println("Basic:[ERROR] " + e.getMessage());
+        }
+
+        if(registry == null || basic == null) {
+            System.err.println("Basic:Could not connect to registry or could not get lookup results!");
+        }
+
+        int a = new Random().nextInt(10),
+                b = new Random().nextInt(10);
+        String  s1 = Client.gstr(3, false),
+                s2 = Client.gstr(3, false),
+                s3 = Client.gstr(7, true);
+        try {
+            System.out.println("Basic:rmiSquare(" + a + ") = " + basic.rmiSquare(a));
+            System.out.println("Basic:rmiAdd(" + a + ", " + b + ") = " + basic.rmiAdd(a, b));
+            System.out.println("Basic:rmiConcat(\"" + s1 + "\", \"" + s2 + "\") = " + basic.rmiConcat(s1, s2));
+            System.out.println("Basic:rmiSplit(\"" + s3 + "\") = " + basic.rmiSplit(s3));
+
+            System.out.println("Basic:rmiIncrement()");
+            basic.rmiIncrement();
+
+            if (c == 2) {
+                System.out.println("Basic:rmiShutdown()");
+                basic.rmiShutdown();
+            }
+
+            System.out.println("Basic:rmiServernameAtWithException(" + a + ") = " + basic.rmiServernameAtWithException(a));
+        } catch (RemoteException e) {
+            System.err.println("Basic:[ERROR] " + e.getMessage());
+        }
+    }
+
 
     private static void call_basicplus(String namespace, int port, int c) {
         Registry registry = null;
@@ -66,7 +106,52 @@ public class Client {
         }
     }
 
+    private static void call_extended(String namespace, int port, int c) {
+        Registry registry = null;
+        ExtendedIf extended = null;
 
+        try {
+            registry = LocateRegistry.getRegistry(port);
+            extended = (ExtendedIf) registry.lookup( "a4-" + namespace );
+        } catch (RemoteException | NotBoundException e) {
+            System.err.println("Extended:[ERROR] " + e.getMessage());
+        }
+
+        if(registry == null || extended == null) {
+            System.err.println("Extended:Could not connect to registry or could not get lookup results!");
+        }
+
+        int a = new Random().nextInt(10),
+                b = new Random().nextInt(10);
+        String  s1 = Client.gstr(3, false),
+                s2 = Client.gstr(3, false),
+                s3 = Client.gstr(7, true);
+
+        try {
+            System.out.println("Extended:rmiSquare(" + a + ") = " + extended.rmiSquare(a));
+            System.out.println("Extended:rmiAdd(" + a + ", " + b + ") = " + extended.rmiAdd(a, b));
+            System.out.println("Extended:rmiConcat(\"" + s1 + "\", \"" + s2 + "\") = " + extended.rmiConcat(s1, s2));
+            System.out.println("Extended:rmiSplit(\"" + s3 + "\") = " + extended.rmiSplit(s3));
+
+            System.out.println("Extended:rmiIncrement()");
+            extended.rmiIncrement();
+
+
+            System.out.println("Extended:rmiQuad(" + a + ") = " + extended.rmiQuad(a));
+            System.out.println("Extended:rmiTwice(\"" + s1 + "\") = " + Arrays.toString(extended.rmiTwice(s1)));
+            System.out.println("Extended:rmiReaddir(\".\") = " + extended.rmiReaddir("."));
+
+            if (c == 2) {
+                System.out.println("Extended:rmiShutdown()");
+                extended.rmiShutdown();
+            }
+
+            System.out.println("Extended:rmiServernameAtWithException(" + a + ") = " + extended.rmiServernameAtWithException(a));
+
+        } catch (RemoteException e) {
+            System.err.println("Extended:[ERROR] " + e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         Client.call_basicplus("basic", 2500, 1);
